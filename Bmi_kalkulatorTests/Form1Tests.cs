e3c9ac39905af3d149d11104faa600055c7cea28
lajki <barnasgrzesiek@gmail.com>
2020-07-03 20:01:14 +0200

    Add new files

---
+++ Bmi_kalkulatorTests/Form1Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bmi_kalkulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bmi_kalkulator.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void Form1Test()
+        {
+
+            
+            double wiek = 18;
+            double w = 100;
+            double h = 100;
+            double wynik = w / (h * h) * 10000;
+
+
+
+
+            Assert.AreEqual(100, 100);
+        }
+
+    }
+
+       
+    }
